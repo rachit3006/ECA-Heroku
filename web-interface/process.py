24c4e9376ecde0d9777eeb40cb51650d18fb1689
rachit3006 <arachit3006@gmail.com>
2022-07-14 23:48:39 +0530

    changes

--- web-interface/process.py
+++ web-interface/process.py
@@ -5,22 +5,26 @@ from time import sleep
 from flask_socketio import SocketIO, emit
 import time
 import io
-import base64,cv2
+import base64, cv2
 from keras.preprocessing import image
 import numpy as np
 from tensorflow.keras.utils import img_to_array
 from engineio.payload import Payload
 import os
+# from feat import Detector
 
 Payload.max_decode_packets = 2048
 
 app = Flask(__name__)
 socketio = SocketIO(app,cors_allowed_origins='*' )
 
+# app.config["UPLOAD_DIR"] = "/home/kumar/RachitVyom/ECA/ECA Using Socket/web-interface/images"
+
+# detector = Detector()
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index1.html')
-
 
 def readb64(base64_string):
     idx = base64_string.find('base64,')
@@ -30,7 +34,9 @@ def readb64(base64_string):
 
     sbuf.write(base64.b64decode(base64_string, ' /'))
     pimg = Image.open(sbuf)
+    # pimg.save(os.path.join(app.config["UPLOAD_DIR"], "download.jpg"))
 
+    # return os.path.join(app.config["UPLOAD_DIR"], "download.jpg")
     return cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
 
 def moving_average(x):
@@ -46,8 +52,8 @@ prev_recv_time = 0
 cnt=0
 fps_array=[0]
 
-face_classifier = cv2.CascadeClassifier(r'/home/vyom/MAIN/Summer Internship/ECA Using Socket/web-interface/haarcascade_frontalface_default.xml')
-classifier = load_model(r'/home/vyom/MAIN/Summer Internship/ECA Using Socket/web-interface/model.h5')
+face_classifier = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+classifier = load_model('./model.h5')
 
 emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
 
@@ -58,9 +64,47 @@ def image(data_image):
     text  =  'FPS: '+str(fps)
     frame = (readb64(data_image))
 
+    # img_path = (readb64(data_image))
+    # results = detector.detect_image(img_path)
+
+    # emotions_values = {'Anger': results['anger'][0],
+    #                 'Disgust': results['disgust'][0],
+    #                 'Fear': results['fear'][0],
+    #                 'Happiness': results['happiness'][0],
+    #                 'Sadness': results['sadness'][0],
+    #                 'Surprise': results['surprise'][0],
+    #                 'Neutral': results['neutral'][0]}
+
+    # emotion = max(emotions_values, key=lambda x: emotions_values[x])
+    # engagement = ""
+
+    # CI = 0
+
+    # if emotion=='Neutral':
+    #     CI = emotions_values['Neutral']*0.9
+    # elif emotion=='Happiness':
+    #     CI = emotions_values['Happiness']*0.6
+    # elif emotion=='Surprise':
+    #     CI = emotions_values['Surprise']*0.6
+    # elif emotion=='Sadness':
+    #     CI = emotions_values['Sadness']*0.3
+    # elif emotion=='Disgust':
+    #     CI = emotions_values['Disgust']*0.2
+    # elif emotion=='Anger':
+    #     CI = emotions_values['Anger']*0.25
+    # else:
+    #     CI = emotions_values['Fear']*0.3
+
+    # if CI >= 0.5 and CI <= 1:
+    #     engagement = 'Engaged'
+    # else:
+    #     engagement = 'Not Engaged'
     labels = []
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray)
+
+    emotion = ""
+    engagement = ""
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
@@ -124,6 +168,4 @@ def image(data_image):
         cnt=0
     
 if __name__ == '__main__':
-    socketio.run(app,port=9990 ,debug=True)
-   
-
+    socketio.run(app,port=3000 ,debug=True)
